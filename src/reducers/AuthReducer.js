67d077c50b04b4d45213016cdff93d84c633ad2b
rochafialvin <rochafi.dev@gmail.com>
2019-07-16 10:59:41 +0700

    First init

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,15 @@
+const init = {
+    id: '',
+    name: ''
+}
+
+export default (data = init, action) => {
+    switch (action.type) {
+        case 'LOGIN_SUCCESS':
+            
+            break;
+    
+        default:
+            return data
+    }
+}
