86e6f2d68d835c1d82e127758ce5b0588fd3f2af
rochafialvin <rochafi.dev@gmail.com>
2019-07-16 11:13:56 +0700

    start R L H

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Register from './Register'
+import Login from './Login'
+import Home from './Home'
 
 class App extends Component {
     render() {
         return (
-            <h1>App Component</h1>
+            <BrowserRouter>
+                <Route path='/' exact component={Home}/>
+                <Route path='/register' component={Register}/>
+                <Route path='/login' component={Login}/>
+            </BrowserRouter>
         )
     }
 }
